3b15b974871fa518f3214474bdd6866413588797
SusanaER <susana.esparza1213@gmail.com>
2023-01-15 00:06:22 -0600

    Variables

---
+++ 00_hello.py
@@ -0,0 +1,16 @@
+#Esto es un comentario de una linea
+"""
+Esto se supone que es un comentario
+donde se tienen mas lineas pero
+no esta funcionando,
+creo que si funciona pero se ve diferente
+"""
+#Hola mundo
+print('Hola mundo')
+
+#Consultar el tipo de dato
+print(type('Hola mundo'))
+print(type(5))
+print(type(5.1))
+print(type(True))
+print(type(1+5j))
---
+++ 01_variables.py
@@ -0,0 +1,19 @@
+string_variable = "Variable tipo string"
+print(string_variable)
+
+int_variable = 5
+print(int_variable)
+
+bool_variable = False
+print(bool_variable)
+
+#Concatenar variables
+print(string_variable, int_variable, bool_variable)
+
+#Convertir datos
+print(type(int_variable))
+int_to_string = str(int_variable)
+print(type(int_to_string))
+
+#Contar caracteres en un string
+print(len(string_variable))
